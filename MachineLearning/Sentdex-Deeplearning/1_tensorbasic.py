85cb5a837b5e691a2e4965103ffdb243a03fce0b
박도연 <park.doyeonn@gmail.com>
2019-04-13 14:53:34 +0900

    텐서플로 1강 완료

--- MachineLearning/Sentdex-Deeplearning/1_tensorbasic.py
+++ MachineLearning/Sentdex-Deeplearning/1_tensorbasic.py
@@ -3,4 +3,21 @@ import tensorflow as tf
 x1 = tf.constant(5)
 x2 = tf.constant(6)
 
-result = x1*x2
+result = tf.multiply(x1, x2)
+#result=x1*x2 위 아래 두개 다 같은 결과값 출력 가능
+
+#print(result) 세션이 열리지 않아서 출력되지 않음
+sess = tf.Session() 
+
+#print(sess.run(result))
+with tf.Session() as sess:
+    output = sess.run(result)
+    print(output)
+
+print(output) #세션이 닫혀도 레퍼런스 결과가 출력됨
+# print(sess.run(result)) 하지만 닫힌 오브젝트는 출력될 수 없음
+
+# sess.close()
+# session은 connection object이다
+
+
