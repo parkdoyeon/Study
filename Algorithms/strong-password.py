30b09d4cb61080c4e4528fc8f50034ae5d420971
박도연 <park.doyeonn@gmail.com>
2019-06-23 12:57:01 +0900

    설명 추가

--- Algorithms/strong-password.py
+++ Algorithms/strong-password.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+# https://www.hackerrank.com/challenges/strong-password/problem
 
 import math
 import os
@@ -21,12 +21,22 @@ def minimumNumber(n, password):
         point += 1
     if not re.search('[A-Z]', password):
         point += 1
-    if not re.search('[!@#$%^&*()\-+]', password):
+    if not re.search('[!@#$%^&*()\-+]', password): #'-' escpae 빼먹지 말것
         point += 1
     if point + n < 6:
         point += 6-(point+n)
     
     return point
+
+# 조금 더 깔끔한 로직
+# def minimumNumber(n, password):
+#     count = 0
+#     cases = [r'[a-z]', r'[A-Z]', r'[\d]', r'[!@#$%^&*()\-+]']
+#     for case in cases:
+#         if not re.search(case, password):
+#             count += 1
+
+#     return max(count, 6 - n)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
