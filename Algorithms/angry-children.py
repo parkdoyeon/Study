fcde12d904fe01949be07eab8d92cdb3b0fafd26
박도연 <park.doyeonn@gmail.com>
2019-07-21 01:10:30 +0900

    주석추가

--- Algorithms/angry-children.py
+++ Algorithms/angry-children.py
@@ -1,5 +1,3 @@
-#https://www.hackerrank.com/challenges/angry-children
-
 #!/bin/python3
 
 import math
@@ -12,23 +10,17 @@ import sys
 def maxMin(k, arr):
     arr.sort()
     ret = []
-    example = []
     for i in range(len(arr)-k+1):
-        k_list = arr[i:i+k]
-        example.append(k_list[-1]-k_list[0])
-        if chkFair(k_list):
-            ret.append(k_list[-1]-k_list[0])
-    print(example)
+        #k_list = arr[i:i+k]  이렇게 했더니 복사하는 과정에서 인덱싱이 일어나는게 부하를 주는지 타임아웃 3건이 발생했다.
+        #ret.append(k_list[-1]-k_list[0])
+
+        # 직접 접근으로 연산 했더니 성공함 
+        ret.append(arr[i+k-1]-arr[i])
     return min(ret)
 
-def chkFair(karr):
-    gap = karr[1]-karr[0]
-    for i in range(1, len(karr)-1):
-        if karr[i+1]-karr[i] != gap:
-            return False
-    return True
-
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     n = int(input())
 
     k = int(input())
@@ -41,4 +33,6 @@ if __name__ == '__main__':
 
     result = maxMin(k, arr)
 
-    print(str(result) + '\n')
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
