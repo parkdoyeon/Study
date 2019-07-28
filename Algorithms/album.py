77db3028762d16c966e5aa7528b3953530aecb3a
박도연 <park.doyeonn@gmail.com>
2019-07-28 15:55:59 +0900

    album 수정

--- Algorithms/album.py
+++ Algorithms/album.py
@@ -19,19 +19,21 @@
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 500, 2500]
 
-albumrank = sorted([(v,k) for k,v in dict(zip(genres, plays)).items()], reverse=True)
-album_bysong = { key : list() for key in genres }
-for i in range(len(genres)):
-    album_bysong[genres[i]] += [[plays[i],i]]
-
-
+albumrank = dict.fromkeys(genres, 0)
+album_songs = { g : list() for g in genres }
+for i in range(len(plays)):
+    albumrank[genres[i]] += plays[i]
+    album_songs[genres[i]] += [(plays[i], i)]
+albumrank_tuple = [(albumrank[g], g) for g in albumrank.keys()]
+albumrank_tuple.sort(reverse=True)
+print(album_songs)
 answer = []
-for g in albumrank:
-    album_bysong[g[1]].sort(key=lambda x: (x[0], -x[1]))
-    print(album_bysong)
+for play, genre in albumrank_tuple:
     count = 1
-    while count < 3 and len(album_bysong[g[1]]) > 0:
-        answer.append(album_bysong[g[1]].pop()[1])
+    album_songs[genre].sort(key=lambda x: (x[0], -x[1]))
+    print(album_songs[genre])
+    while count < 3 and len(album_songs[genre]) > 0:
+        answer.append(album_songs[genre].pop()[1])
         count += 1
 
 print(answer)
