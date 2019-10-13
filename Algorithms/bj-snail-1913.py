d994cbf5befbcc39f84380cc8c85f331ef9503cc
박도연 <park.doyeonn@gmail.com>
2019-10-13 18:41:44 +0900

    달팽이 순열 수정

--- Algorithms/bj-snail-1913.py
+++ Algorithms/bj-snail-1913.py
@@ -12,34 +12,38 @@ flag = N*N
 dr[middle][middle] = num
 dr[0][0] = flag
 print_two_d(dr)
-while num is not flag:
+while num < flag:
     # up
-    col = col+up
-    num += 1
-    dr[col][row] = num
-    up = up+1 if up > 0 else up-1
-    print_two_d(dr)
-
+    for i in range(abs(up)):
+        col += 1 if up > 0 else -1
+        num += 1
+        if num >= flag: break
+        dr[col][row] = num
+        print_two_d(dr)
+    up = up+2 if up > 0 else up-2
+    #print_two_d(dr)
+    if num >= flag :break
     # right
-    row = row+right
-    num += 1
-    dr[col][row] = num
-    right = right+1 if right > 0 else right-1
-    print_two_d(dr)
-
-    # bottom
-    for i in range(bottom):
-        col = col+1
+    for i in range(abs(right)):
+        row += 1 if right > 0 else -1
         num += 1
         dr[col][row] = num
-        bottom = bottom+1 if bottom > 0 else bottom-1
-        print_two_d(dr)
+    right = right+2 if right > 0 else right-2
+
+    # bottom
+    for i in range(abs(bottom)):
+        col += 1 if bottom > 0 else -1
+        num += 1
+        dr[col][row] = num
+    bottom = bottom+2 if bottom > 0 else bottom-2
+    #print_two_d(dr)
 
     # left
-    row = row+left
-    num += 1
-    dr[col][row] = num
-    left = left+1 if left > 0 else left-1
-    print_two_d(dr)
+    for i in range(abs(left)):
+        row += 1 if left > 0 else -1
+        num += 1
+        dr[col][row] = num
+    left = left+2 if left > 0 else left-2
+    #print_two_d(dr)
 
 print_two_d(dr)
