70d42d0e3fa60a33ff8963b986f776d2939b2251
박도연 <park.doyeonn@gmail.com>
2019-10-13 20:32:54 +0900

    할당구문을 최소화하면 속도 차이가 분명히 있음

--- Algorithms/bj-pibonacci-11443.py
+++ Algorithms/bj-pibonacci-11443.py
@@ -1,19 +1,16 @@
 #1000000000000000000
 N = int(input())
-# one, two, three = (0,1,1)
-# idx = 3
-# even_sum = 1
-# while idx <= N+1:
-#     temp = three
-#     three = two+three
-#     one = two
-#     two = temp
-#     idx += 1
-# print(three-1)
+a,b,n = (0,1,1)
+idx = 3
+even_sum = 1
+while idx <= N:
+    n = a+b
+    a = b
+    b = n
+    idx += 1
+# def fibo(n):
+#     if n is 0: return 0
+#     if n is 1: return 1
+#     return fibo(n-1)+fibo(n-2)
 
-def fibo(n):
-    if n is 0: return 0
-    if n is 1: return 1
-    return fibo(n-1)+fibo(n-2)
-
-print(fibo(N+1)-1)
+print(n)
