bb8aaf957b7ba46354b0c2c60af5cb6b6e790bbb
박도연 <park.doyeonn@gmail.com>
2019-11-16 23:18:44 +0900

    inordertraversal 재귀 사용하지 않고 순회

--- Algorithms/leetcode/inorderTraversal.py
+++ Algorithms/leetcode/inorderTraversal.py
@@ -20,3 +20,35 @@ class Solution:
             self._inorderTraversal(node.left, ans)
             ans.append(node.val)
             self._inorderTraversal(node.right, ans)
+    
+    def inorderTraversal2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        """
+        # Recursive Soltion just for the heck of it
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+        """
+        # Iterative Solution
+        res = []
+        stack = []
+        done = True
+        if root is None:
+            return []
+        curr = root
+        while done:
+            if curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                if len(stack) >0:
+                    curr = stack.pop()
+                    res.append(curr.val)
+                    curr = curr.right
+                else:
+                    done = False
+        return res
+            
